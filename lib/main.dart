2ac7ebcc769c2f570a10cd52f410d62c3f5e676a
willian-virgilio <willian.virgilio@gmail.com>
2021-12-27 14:14:48 -0300

    Default flutter project, only the source code modified

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:catanddogpredictor/splashscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cat vs Dog',
+      home: MySlash(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,34 @@
+import 'package:catanddogpredictor/home.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySlash extends StatefulWidget {
+  const MySlash({Key? key}) : super(key: key);
+
+  @override
+  _MySlashState createState() => _MySlashState();
+}
+
+class _MySlashState extends State<MySlash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: Home(),
+      title: Text(
+        'Dog and Cat',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+          color: Color(
+            0xFFE99600,
+          ),
+        ),
+      ),
+      image: Image.asset('assets/cat.png'),
+      photoSize: 50.0,
+      backgroundColor: Colors.black12,
+      loaderColor: Color(0xFFEEDA28),
+    );
+  }
+}
